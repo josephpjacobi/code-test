4c29d12908b2f752de69f383f0ff56588ae483e8
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-01-16 17:52:04 -0500

    haversine component works with hardcoded data. It takes two zip codes and corresponding coordinates and displays the distance between 2 points in miles (#5)

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const zipCodeData = {
+  00705 {
+    zipCodeType: "STANDARD",
+    city: 'AIBONITO',
+    state: 'PR',
+    locationType: 'PRIMARY',
+    lat: 18.14,
+    long: -66.26,
+    location: "NA-US-PR-AIBONITO"
+  }
+}
