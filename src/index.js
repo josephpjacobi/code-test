376d70b7691ecd40db6a24c6173ee1e1c940bd30
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-01-16 13:09:10 -0500

    String display formatting (#3)
    * formatted reverse string component
    
    * formatting added for StringDisplay and UserInput components

--- src/index.js
+++ src/index.js
@@ -3,22 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 //UserInput accepts a user input which is passed to the ReverseString component
-const UserInput = ({ newValue, handleInput }) => (
-    <input type="text" value={newValue} onChange={handleInput} />
+const UserInput = ({ newValue, handleInput }) => {
+  return (
+    <div className="container">
+      <h3>Enter some text below:</h3>
+      <input type="text" value={newValue} onChange={handleInput} maxLength='50'/>
+    </div>
   )
+}
 
 //ReverseString accepts a string as a prop and displays it in reverse
 const ReverseString = ({ string }) => {
   const stringInReverse = string.split("").reverse().join("")
   return (
-    <div>
+    <div className="container">
+      <h3>Your text in reverse:</h3>
       {stringInReverse}
     </div>
   )
 }
 
 //Display component contains the input and the Reserse String Component
-const Display = () => {
+const StringDisplay = () => {
   const [input, setInput] = useState('');
 
   const handleUserInput = event => {    
@@ -26,13 +32,13 @@ const Display = () => {
   };
 
   return (
-    <div>
+    <div className="display">
       <UserInput newValue={input} handleInput={handleUserInput}/>
       <ReverseString string={input}/>
     </div>
   )
 }
 
-ReactDOM.render(<Display />, document.querySelector('#root'));
+ReactDOM.render(<StringDisplay />, document.querySelector('#root'));
 
 
