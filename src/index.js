2fbda71b7434df71b5b89c4a1e0274e04692f9b6
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-01-16 14:06:44 -0500

    added MainDisplay component to store and display all components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom';
 import StringDisplay from '../src/Components/string-display/string-display.js';
 import './index.css';
 
+//MainDisplay is the hub for storing each component in the code test and the corresponding titles for each section
+const MainDisplay = () => {
+  return (
+    <div>
+      <div className="react-questions">
+        <h1>React Questions</h1>
+        <StringDisplay />
+      </div>
+    </div>
+  )
+}
 
 
-
-
-ReactDOM.render(<StringDisplay />, document.querySelector('#root'));
+ReactDOM.render(<MainDisplay />, document.querySelector('#root'));
 
 
