06f3ce4c3e90723fefc1321339279b75329b1d74
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-01-22 13:06:19 -0500

    processArray function works

--- src/Components/array-filter/array-filter.jsx
+++ src/Components/array-filter/array-filter.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import './array-filter.css';
 
 export const ArrayDisplay = () => {
-  const array = [1, 2, 3, 'a', 'b', 'c'];
+  const arrayToProcess = [1, 2, 'a', 3, 'b', 'c', true, 4, false, '6'];
+  const processArray = () => {   
+    for (let i = arrayToProcess.length - 1; i >= 0; i--) {
+      if (typeof arrayToProcess[i] !== 'number') {
+        arrayToProcess.splice(i, 1);
+      }
+    }
+    alert(`After removing all non-numeric characters, your array is: ${arrayToProcess}`);
+    return arrayToProcess;
+  }
+
   return (
     <div>
-      {array}
+      {arrayToProcess}
+      <button onClick={processArray}>Remove Non-numbers</button>
     </div>
   )
 }
