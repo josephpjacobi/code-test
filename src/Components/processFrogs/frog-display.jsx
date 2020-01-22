1af4c1326123cb257728e2d3db2c72f079c884ef
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-01-22 15:23:50 -0500

    accumulator works for calculating the totale weight of the community when >10

--- src/Components/processFrogs/frog-display.jsx
+++ src/Components/processFrogs/frog-display.jsx
@@ -3,11 +3,46 @@ import './frog-display.css';
 
 export const FrogDisplay = () => {
   const [numberOfDays, setNumberOfDays] = useState('2');
+  const [totalWeight, calcTotal] = useState('');
+  const [averageWeight, calcAverage] = useState('')
+
+  const calculateTotalWeight = (num) => {
+      let total = 0;
+      for (let i = 1; i <= num; i++) {
+        total += i;
+      }
+      const communityWeight = total * 10;
+      console.log(communityWeight);
+      return communityWeight;
+    }
+  //   // if (num <= 10) {
+  //     const days = num;
+  //     let total = 0;
+  //     for(let frogs = 0; frogs <= days; frogs++) {
+  //       let total = total + days;
+  //     }
+  //     console.log(total)
+  // }
+  
+  const handleChange = (event) => {
+    setNumberOfDays(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    calculateTotalWeight(numberOfDays);
+    alert('done!');
+  }
 
   return (
     <div className="frog-data-container">
-      <h2>Enter Number of Days Below:</h2>
-      <input type="text" pattern="[0-9]*" />
+      <form onSubmit={handleSubmit} className="form">
+        <label>
+          Enter Number of Days:
+          <input type="type" value={numberOfDays} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
       <h4>The weight of the frog community on day {numberOfDays} : </h4>
       <h4>The weight of the average frog on day {numberOfDays}:</h4>
     </div>
