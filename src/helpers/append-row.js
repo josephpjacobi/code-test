a50a5699cd7622a01a720f686ab1724f1e713338
Joe Jacobi <joseph.p.jacobi@gmail.com>
2020-01-23 19:27:27 -0500

    Edits refactor (#13)
    * refactored code, made slight edits
    
    * refactored haversine function

--- src/helpers/append-row.js
+++ src/helpers/append-row.js
@@ -1,13 +1,13 @@
 export const appendRow = () => {
-  const table = document.getElementById("tbl")
-  const rowsArray = table.getElementsByTagName('tr')
-  const numberOFRows = rowsArray.length
-  const lastRow = rowsArray[numberOFRows - 1]
+  const table = document.getElementById("tbl");
+  const rowsArray = table.getElementsByTagName('tr');
+  const numberOFRows = rowsArray.length;
+  const lastRow = rowsArray[numberOFRows - 1];
   const lastRowCellCount = lastRow.getElementsByTagName('td').length
   const newRow = table.insertRow();  //defaults to -1 which is the last row
   for (let cellCount = 0; cellCount < lastRowCellCount; cellCount++ ) {
     let newCell = newRow.insertCell();
-    newCell.innerHTML = `New Cell, who dis?`
+    newCell.innerHTML = `New Cell, who dis?`;
   }
 }
 
--- src/helpers/haversine.js
+++ src/helpers/haversine.js
@@ -3,6 +3,22 @@ import { pi, round, asin, pow, sin, cos, sqrt } from 'mathjs';
 
 const RadiusKM = 6371;
 
+const convertToRadians = (coordiante) => {
+  return ((coordiante / 180) * pi)
+}
+
+const distanceInKM = (dLat, dLong, radLat1, radLat2) => {
+  return (RadiusKM * 2 *
+      asin(
+        sqrt(
+          pow(sin(dLat / 2), 2) +
+          pow(sin(dLong / 2), 2) *
+          cos(radLat1) * cos(radLat2)
+        )
+      ) * 100
+    ) / 100;
+}
+
 //Uses the Haversine formula to calculate the great-circle distance between the coordiantes derived from the zip codes
 //The result is converted to miles and rounded to the nearest whole number
 export const Haversine = (startingZip, endingZip) => {
@@ -13,26 +29,16 @@ export const Haversine = (startingZip, endingZip) => {
 
   if (zip1 && zip2) {
     //convert degrees to radians
-    const radLat1 = (zip1.lat / 180) * pi;
-    const radLong1 = (zip1.long / 180) * pi;
-    const radLat2 = (zip2.lat / 180) * pi;
-    const radLong2 = (zip2.long / 180) * pi;
+    const radLat1 = convertToRadians(zip1.lat);
+    const radLong1 = convertToRadians(zip1.long);
+    const radLat2 = convertToRadians(zip2.lat);
+    const radLong2 = convertToRadians(zip2.long);
 
     //Difference in radians
     const dLat = (radLat2 - radLat1);
     const dLong = (radLong2 - radLong1);
-   
-
-    const distanceKM = (
-      RadiusKM * 2 *
-      asin(
-        sqrt(
-          pow(sin(dLat / 2), 2) +
-          pow(sin(dLong / 2), 2) *
-          cos(radLat1) * cos(radLat2)
-        )
-      ) * 100
-    ) / 100;
+    
+    const distanceKM = distanceInKM(dLat, dLong, radLat1, radLat2);
 
     //Converts distance from km to miles and rounds to the nearest whole number
     const distanceInMiles = round(distanceKM * .621);
